a254888216767e14fa6b75620c3074732872f57a
jghuynh <jghuynh@dons.usfca.edu>
2020-02-13 18:51:15 -0800

    Starting config from scratch

--- config1.js
+++ config1.js
@@ -1,61 +1,63 @@
-// configuration of svg/plot area
-let config = {
-  'svg': {},
-  'margin': {},
-  'plot': {}
-};
-
-config.svg.height = 300;
-config.svg.width = config.svg.height * 1.618; // golden ratio
-
-config.margin.top = 10;
-config.margin.right = 10;
-config.margin.bottom = 20;
-config.margin.left = 80;
-
-config.plot.x = config.margin.left;
-config.plot.y = config.margin.top;
-config.plot.width = config.svg.width - config.margin.left - config.margin.right;
-config.plot.height = config.svg.height - config.margin.top - config.margin.bottom;
-
-// setup svg
-let svg = d3.select('body').select('svg');
-svg.attr('width', config.svg.width);
-svg.attr('height', config.svg.height);
-
-// setup plot area
-let plot = svg.append('g');
-plot.attr('id', 'plot');
-//plot.attr('transform', translate(config.plot.x, config.plot.y));
-
-// use a rect to illustrate plot area
-let rect = plot.append('rect');
-rect.attr('id', 'background');
-
-rect.attr('x', 0);
-rect.attr('y', 0);
-rect.attr('width', config.plot.width);
-rect.attr('height', config.plot.height);
-
-// scales for data
-let scale = {};
-
-scale.x = d3.scaleBand();
-scale.x.range([0, config.plot.width]);
-
-scale.y = d3.scaleBand();
-scale.y.range([config.plot.height, 0]);
-
-// https://github.com/d3/d3-scale-chromatic
-scale.color = d3.scaleSequential(d3.interpolateViridis);
-
-let axis = {};  // axes for data
-axis.x = d3.axisBottom(scale.x);
-axis.x.tickPadding(0);
-
-axis.y = d3.axisLeft(scale.y);
-axis.y.tickPadding(0);
-
-// format the tick labels
-axis.x.tickFormat(dateFormatter);
-axis.y.tickFormat(regionFormatter);
+// // configuration of svg/plot area
+// let config = {
+//   'svg': {},
+//   'margin': {},
+//   'plot': {}
+// };
+//
+// config.svg.height = 400;
+// config.svg.width = config.svg.height * 1.618; // golden ratio
+//
+// // the margins
+// config.margin.top = 10;
+// config.margin.right = 10;
+// config.margin.bottom = 20;
+// config.margin.left = 80;
+//
+// // The plot dimensions
+// config.plot.x = config.margin.left;
+// config.plot.y = config.margin.top;
+// config.plot.width = config.svg.width - config.margin.left - config.margin.right;
+// config.plot.height = config.svg.height - config.margin.top - config.margin.bottom;
+//
+// // setup svg
+// let svg = d3.select('body').select('svg');
+// svg.attr('width', config.svg.width);
+// svg.attr('height', config.svg.height);
+//
+// // setup plot area
+// let plot = svg.append('g');
+// plot.attr('id', 'plot');
+// //plot.attr('transform', translate(config.plot.x, config.plot.y));
+//
+// // use a rect to illustrate plot area
+// let rect = plot.append('rect');
+// rect.attr('id', 'background');
+//
+// rect.attr('x', 0);
+// rect.attr('y', 0);
+// rect.attr('width', config.plot.width);
+// rect.attr('height', config.plot.height);
+//
+// // scales for data
+// let scale = {};
+//
+// scale.x = d3.scaleContinuous();
+// scale.x.range([0, config.plot.width]);
+//
+// scale.y = d3.scaleContinuous();
+// scale.y.range([config.plot.height, 0]);
+//
+// // https://github.com/d3/d3-scale-chromatic
+// scale.color = d3.scaleSequential(d3.interpolateViridis);
+//
+// let axis = {};  // axes for data
+// axis.x = d3.axisBottom(scale.x);
+// axis.x.tickPadding(0);
+//
+// axis.y = d3.axisLeft(scale.y);
+// axis.y.tickPadding(0);
+//
+// // format the tick labels
+// axis.x.tickFormat(dateFormatter);
+// axis.y.tickFormat(regionFormatter);
--- lineGraph.js
+++ lineGraph.js
@@ -1,3 +1,20 @@
+// configurations from bubbles
+
+const width = 960;
+const height = 500;
+
+const margin = {
+  top: 10,
+  bottom: 35,
+  left: 35,
+  right: 15
+}
+
+
+
+
+
+
 let parseTime = d3.timeParse("%Y%m");
 myTime = parseTime(197912);
 console.log("Here it is! ");
@@ -51,8 +68,78 @@ for (let col in row) {
 //console.log(out.values[0]); --> the dates
 return out;
 } // function parseData
-
+airData = d3.csv("Air_Traffic_Passenger_Statistics.csv", parseData)
+.then(function(data) {
+  //console.log("Here is airData: ", airData);
+  // parse data
+  //var myParsedData = parseData(data)
+  // now graph
+  drawLineGraph(data);
+});
 //}
+// configuration of svg/plot area
+// let config = {
+//   'svg': {},
+//   'margin': {},
+//   'plot': {}
+// };
+//
+// config.svg.height = 400;
+// config.svg.width = config.svg.height * 1.618; // golden ratio
+//
+// // the margins
+// config.margin.top = 10;
+// config.margin.right = 10;
+// config.margin.bottom = 20;
+// config.margin.left = 80;
+//
+// // The plot dimensions
+// config.plot.x = config.margin.left;
+// config.plot.y = config.margin.top;
+// config.plot.width = config.svg.width - config.margin.left - config.margin.right;
+// config.plot.height = config.svg.height - config.margin.top - config.margin.bottom;
+//
+// // setup svg
+// let svg = d3.select('body').select('svg');
+// svg.attr('width', config.svg.width);
+// svg.attr('height', config.svg.height);
+//
+// // setup plot area
+// let plot = svg.append('g');
+// plot.attr('id', 'plot');
+// //plot.attr('transform', translate(config.plot.x, config.plot.y));
+//
+// // use a rect to illustrate plot area
+// let rect = plot.append('rect');
+// rect.attr('id', 'background');
+//
+// rect.attr('x', 0);
+// rect.attr('y', 0);
+// rect.attr('width', config.plot.width);
+// rect.attr('height', config.plot.height);
+//
+// // scales for data
+// let scale = {};
+//
+// scale.x = d3.scaleLinear();
+// scale.x.range([0, config.plot.width]);
+//
+// scale.y = d3.scaleLinear();
+// scale.y.range([config.plot.height, 0]);
+//
+// // https://github.com/d3/d3-scale-chromatic
+// scale.color = d3.scaleSequential(d3.interpolateViridis);
+//
+// let axis = {};  // axes for data
+// axis.x = d3.axisBottom(scale.x);
+// axis.x.tickPadding(0);
+//
+// axis.y = d3.axisLeft(scale.y);
+// axis.y.tickPadding(0);
+//
+// // format the tick labels
+// axis.x.tickFormat(dateFormatter);
+// axis.y.tickFormat(regionFormatter);
 
 let drawLineGraph = function(data) {
   console.log(data);
@@ -95,35 +182,19 @@ let drawLineGraph = function(data) {
 
   let regions = data.map(row => row["GEO Region"]);
   // so every row is a region
-  console.log(regions);
-  console.log(data);
+  //console.log(regions);
+  //console.log(data);
   //console.log(data.map); does nothing
   // not the best design, but it works and we're moving on
   let years = data.map(row => parseTime(row["Activity Period"]));
-  console.log("data[0] = ", data[0]);// only 1 row
+  //console.log("data[0] = ", data[0]);// only 1 row
   // let years = data[0].values.map(value => value.date);
   //console.log(value.date);
 
-  console.log(years);
+  //console.log(years);
   //console.log(years.length);
 
   // now we have data, set the scales
-  scale.x.domain(years);
-  scale.y.domain(regions);
-
-
-  // draw the x and y axis
- let gx = svg.append("g");
- gx.attr("id", "x-axis");
- gx.attr("class", "axis");
- gx.attr("transform", translate(config.plot.x, config.plot.y + config.plot.height));
- gx.call(axis.x);
-
- let gy = svg.append("g");
- gy.attr("id", "y-axis");
- gy.attr("class", "axis");
- gy.attr("transform", translate(config.plot.x, config.plot.y));
- gy.call(axis.y);
 
 // modify svg
 /*
@@ -133,5 +204,57 @@ add in scales
 figure what type of scale: category, continuous, linear
 define domain, range
 */
+
+// Sophie speaks
+// draw the x and y axis
+//   let gx = svg.append("g");
+//   gx.attr("id", "x-axis");
+//   gx.attr("class", "axis");
+//   gx.attr("transform", translate(config.plot.x, config.plot.y + config.plot.height));
+//   gx.call(axis.x);
+//
+//   let gy = svg.append("g");
+//   gy.attr("id", "y-axis");
+//   gy.attr("class", "axis");
+//   gy.attr("transform", translate(config.plot.x, config.plot.y));
+//   gy.call(axis.y);
+//
+// // get all of the value objects (with date and value) from the rows
+//   let values = data.map(d => d.values);
+//   console.log("Here are values: ", values);
+//
+//   // combine all of the individual object arrays into one
+//   let merged = d3.merge(values);
+
+
+  // only want to show short year in labels
+  // https://stackoverflow.com/questions/17306830/how-to-get-2-digit-year-w-javascript
+  function dateFormatter(d) {
+    if (d.getMonth() < 3) {
+      //return "'" + d.getFullYear().toString().substr(-2);
+      return d.getFullYear().toString();
+    }
+  }
+
+  // convert region to more condensed form
+  function regionFormatter(d) {
+    let text = d;
+    let parts = text.split(/[,-]+/);
+
+    if (parts !== null) {
+      text = parts[0];
+
+      if (parts.length > 2) {
+        text = text + "+";
+      }
+    }
+
+    return text;
+  }
+
+  // helper method to make translating easier
+function translate(x, y) {
+  return 'translate(' + x + ',' + y + ')';
+}
 
 };
